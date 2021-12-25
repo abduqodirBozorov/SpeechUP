4b75c0c2a993665d10a4c960f18064719b0dded8
abduqodirBozorov <abduqodirnote@gmail.com>
2021-12-25 11:40:43 +0500

    fullscreen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+// fullscreen
+var body = document.getElementById("body")
+console.log(body);
+
+body.requestFullscreen()
+
+
 var hamburber = document.querySelector(".hamburger")
 var navlist = document.querySelector(".nav-list")
 
