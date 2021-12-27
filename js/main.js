694366b82674f1c5786385ee504b0b9235a39117
abduqodirBozorov <abduqodirnote@gmail.com>
2021-12-27 10:41:42 +0500

    bug fix

--- js/main.js
+++ js/main.js
@@ -7,3 +7,7 @@ console.log(body);
 var hamburber = document.querySelector(".hamburger")
 var navlist = document.querySelector(".nav-list")
 
+hamburber.addEventListener("click", function (e) {
+  e.preventDefault()
+  navlist.classList.toggle("nav-list--open")
+})
