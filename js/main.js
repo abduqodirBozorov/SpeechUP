48d866f4fa20e06ab3e4604baea3f343652622f3
abduqodirBozorov <abduqodirnote@gmail.com>
2021-12-27 10:40:39 +0500

    full screen bug fix

--- js/main.js
+++ js/main.js
@@ -7,8 +7,3 @@ console.log(body);
 var hamburber = document.querySelector(".hamburger")
 var navlist = document.querySelector(".nav-list")
 
-hamburber.addEventListener("click", function (e) {
-  e.preventDefault()
-  navlist.classList.toggle("nav-list--open")
-  body.requestFullscreen()
-})
